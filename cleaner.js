9e3176765d1d370b0c67409487fb1b28446daa76
Steve Conklin <steve@conklinhouse.com>
2013-03-09 23:00:01 -0600

    measurement select works

--- cleaner.js
+++ cleaner.js
@@ -3,28 +3,44 @@ var PatternButton = document.getElementById("SPButton");
 var haveMeasurements = false;
 var havePattern = false;
 
+// Turn off caching, or changes to json files, etc
+// won't get reloaded
+$.ajaxSetup({ cache: false });
+
+// catch the event on the measurements button
 MeasurementsButton.addEventListener("click", function(e){
     e.preventDefault();
     console.log("MButton");
     getMeasurement();
 });
 
+// catch the event on the patterns button
 PatternButton.addEventListener("click", function(e){
     e.preventDefault();
     console.log("PButton");
 });
 
 // Triggered when the user selects a measurement file
+// from the popup
 $("#popupSelectM").change(function(e) {
     console.log("Measurement file CHANGED");
     window.fileSelection = $("#popupSelectM").val();
-    console.log(window.fileSelection);
+    mFileName = window.fileSelection;
+    console.log(mFileName);
     // Now we have a file selected
     $("#selectMeasurementsDiv").hide();
-    $.getJSON(window.fileSelection, function(mdata) {
-	for (var i=0, len=mdata.length; i < len; i++) {
-            console.log(mdata[i]);
+    $.getJSON(mFileName, function(mdata) {
+	// Now we have measurement data
+	window.measurementData = mdata
+	// If we already have a pattern script loaded, 
+	// then call into it
+	if(typeof drawPattern == 'function')
+	{
+	    // The funtion drawPattern exists
+	    drawPattern(window.measurementData);
 	}
+ 	
+	console.log(mdata);
     });
 });
 
@@ -32,18 +48,30 @@ function getMeasurement(){
     // Fetch a list of available Measurements
     // Right now this grabs a list from a json file,
     // but could ultimately query a db
-    $.getJSON("measurement_list.json", function(mdata) {
+    $.getJSON("measurement_list.json", function(mlist) {
+
 	// Now we have a list of files, let the user select one
-	//for (var i=0, len=mdata.length; i < len; i++) {
-        //    console.log(mdata[i]);
+	//for (var i=0, len=mlist.length; i < len; i++) {
+        //    console.log(mlist[i]);
 	//}
+
+	// we'll set up the select list, then display the popup
+	// that contains it
 	var $el = $("#popupSelectM");
 	$el.empty(); // remove old options from the select
-	// Add the new ones
-	$.each(mdata, function(key, value) {
-	    var name = value.slice(0,-5);
+
+	// Add the first prompt. Selecting this one will not trigger
+	// a change, since it is selected already
+	$el.append($("<option></option>")
+		   .attr("value", "select").text("Select Measurements"));
+
+	// Add the choices we read from the json file
+	$.each(mlist, function(key, val) {
+	    // remove the .json from the end
+	    var name = val.slice(0,-5);
+	    // Add the option to the list
 	    $el.append($("<option></option>")
-		       .attr("value", value).text(name));
+		       .attr("value", val).text(name));
 	});
 	$("#selectMeasurementsDiv").show();
 	// Now, when user selects one of these the change callback will happen
