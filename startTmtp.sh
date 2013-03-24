65cadf4f5654c8baa79e1d19b37cb7124632b7c2
Susan Spencer <susan.spencer@gmail.com>
2013-03-24 17:18:51 -0500

    Updated script to open tmtpw.html

--- startTmtp.sh
+++ startTmtp.sh
@@ -1,2 +1,2 @@
 #python -m SimpleHTTPServer 
-chromium-browser --allow-file-access-from-files http://localhost:8000/cleaner.html
+chromium-browser --allow-file-access-from-files http://localhost:8000/tmtpw.html
